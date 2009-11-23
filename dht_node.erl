0d5c765799aad7a82c4cc60f80564bfc1cdf85b6
Astro <astro@spaceboyz.net>
2009-11-23 20:39:44 +0100

    routes reimplementation instead of nodes

--- dht_node.erl
+++ dht_node.erl
@@ -3,20 +3,19 @@
 -behaviour(gen_server).
 
 %% API
--export([start_link/1, start_link/2, ping/2, find_node/2, get_peers/2]).
+-export([start_link/1, start_link/2]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
 	 terminate/2, code_change/3]).
 
--record(state, {sock, node_id, requests, t = 0}).
+-record(state, {sock, node_id, requests, t = 0, routes}).
 -record(request, {t, addr, resend, caller, last_sent, ntry = 0}).
 
 -define(PKT_TIMEOUT, 2000).
 -define(MAX_TRIES, 3).
 
 -define(CALL_TIMEOUT, 8000).
--define(TICK_INTERVAL, 100).
 
 %%====================================================================
 %% API
@@ -27,66 +26,14 @@ ip_port_to_addr({A, B, C, D}, Port) ->
 addr_to_ip_port(<<A, B, C, D, Port:16/big>>) ->
     {{A, B, C, D}, Port}.
 
+addr_to_s(<<A, B, C, D, Port:16/big>>) ->
+    io_lib:format("~B.~B.~B.~B:~B", [A, B, C, D, Port]).
+
 start_link(Port) ->
     start_link(generate_node_id(), Port).
 
 start_link(NodeId, Port) ->
     gen_server:start_link(?MODULE, [NodeId, Port], []).
-
-ping(Pid, Addr) ->
-    gen_server:call(Pid, {request, Addr, <<"ping">>}, ?CALL_TIMEOUT).
-
-find_node(Pid, InfoHash) ->
-    Ask = fun(Addr) ->
-		  catch gen_server:call(Pid,
-					{request, Addr,
-					 <<"find_node">>, [{<<"target">>, InfoHash}]},
-					?CALL_TIMEOUT)
-	  end,
-    find_node1(Ask, InfoHash, []).
-
-find_node1(Ask, InfoHash, Seen) ->
-    Nearest = nodes:get_nearest(InfoHash),
-    %% Generate a list of those who are nearest but not Seen yet
-    ToAsk = lists:foldl(fun(Addr, ToAsk) ->
-				case lists:member(Addr, Seen) of
-				    true -> ToAsk;
-				    false -> [Addr | ToAsk]
-				end
-			end, [], Nearest),
-    io:format("finde node: ~p seen, ~p to ask~n",[length(Seen), length(ToAsk)]),
-    case ToAsk of
-	%% Asked all nearest, but no answer
-	[] -> Nearest;
-	%% Further nodes
-	_ ->
-	    util:pmap(Ask, ToAsk),
-	    find_node1(Ask, InfoHash, ToAsk ++ Seen)
-    end.
-
-get_peers(Pid, InfoHash) ->
-    find_node(Pid, InfoHash),
-    Nodes = nodes:get_nearest(InfoHash, good),
-    io:format("get_peers: ~p nodes to ask for peers~n", [length(Nodes)]),
-    lists:append(
-      util:pmap(
-	fun(Addr) ->
-		case catch gen_server:call(Pid,
-					   {request, Addr,
-					    <<"get_peers">>, [{<<"info_hash">>, InfoHash}]},
-					   ?CALL_TIMEOUT) of
-		    {ok, R} ->
-			case dict_get(<<"values">>, R, false) of
-			    false -> [];
-			    Values ->
-				lists:filter(fun(<<_:6/binary>>) -> true;
-						(_) -> false
-					     end, Values)
-			end;
-		    _ -> []
-		end
-	end, Nodes)
-     ).
 
 %%====================================================================
 %% gen_server callbacks
@@ -94,128 +41,78 @@ get_peers(Pid, InfoHash) ->
 init([NodeId, Port]) ->
     {ok, Sock} = gen_udp:open(Port, [binary, {active, true}]),
     Requests = ets:new(requests, [set, private, {keypos, #request.t}]),
-    {ok, #state{sock = Sock, node_id = NodeId, requests = Requests}, ?TICK_INTERVAL}.
+    next_state(ok, #state{sock = Sock, node_id = NodeId, requests = Requests, routes = routes:new(NodeId)}).
 
 handle_call({request, Addr, Q}, From, State) ->
     handle_call({request, Addr, Q, []}, From, State);
-handle_call({request, {Host, Port}, Q, As}, From, State) ->
-    I = self(),
-    spawn_link(fun() ->
-		       case inet:getaddrs(Host, inet) of
-			   {ok, IPs} ->
-			       IP = lists:nth(random:uniform(length(IPs)), IPs),
-			       Addr = ip_port_to_addr(IP, Port),
-			       Result = gen_server:call(I, {request, Addr, Q, As}, ?CALL_TIMEOUT),
-			       gen_server:reply(From, Result);
-			   _ ->
-			       gen_server:reply(From, {error, nxdomain})
-		       end
-	       end),
-    {noreply, State};
 handle_call({request, Addr, Q, As}, From,
-	    #state{sock = Sock, node_id = NodeId, requests = Requests, t = T1} = State) ->
-    io:format("request ~p to ~p~n", [Q, Addr]),
-    %% {"t":"aa", "y":"q", "q":"ping", "a":{"id":"abcdefghij0123456789"}}
+	    #state{sock = Sock, node_id = NodeId, t = T1} = State) ->
+    I = self(),
     {T, T2} = next_t(T1),
-    Pkt = [{<<"t">>, T},
-	   {<<"y">>, <<"q">>},
-	   {<<"q">>, Q},
-	   {<<"a">>,
-	    [{<<"id">>, NodeId} | As]
-	   }],
-    PktBin = benc:to_binary(Pkt),
-    {IP, Port} = addr_to_ip_port(Addr),
-    Send = fun() ->
-		   gen_udp:send(Sock, IP, Port, PktBin)
-	   end,
-    Request = #request{t = T,
-		       addr = Addr,
-		       resend = Send,
-		       caller = From,
-		       last_sent = util:mk_timestamp_ms()},
+    spawn_link(fun() ->
+		       io:format("request ~p to ~s~n", [Q, addr_to_s(Addr)]),
+		       %% {"t":"aa", "y":"q", "q":"ping", "a":{"id":"abcdefghij0123456789"}}
+		       Pkt = [{<<"t">>, T},
+			      {<<"y">>, <<"q">>},
+			      {<<"q">>, Q},
+			      {<<"a">>,
+			       [{<<"id">>, NodeId} | As]
+			      }],
+		       PktBin = benc:to_binary(Pkt),
+		       {IP, Port} = addr_to_ip_port(Addr),
+		       Send = fun() ->
+				      gen_udp:send(Sock, IP, Port, PktBin)
+			      end,
+		       Request = #request{t = T,
+					  addr = Addr,
+					  resend = Send,
+					  caller = From,
+					  last_sent = util:mk_timestamp_ms()},
+		       gen_server:cast(I, {insert_request, Request}),
+		       Send()
+	       end),
+    next_state(noreply, State#state{t = T2}).
+
+handle_cast({insert_request, Request}, #state{requests = Requests} = State) ->
     ets:insert(Requests, Request),
-    Send(),
-    {noreply, State#state{t = T2}, ?TICK_INTERVAL}.
+    next_state(noreply, State);
 
-handle_cast(_Msg, State) ->
-    {noreply, State, ?TICK_INTERVAL}.
+handle_cast({packet, Addr, Pkt}, State) ->
+    T = dict_get(<<"t">>, Pkt, <<>>),
+    case dict_get(<<"y">>, Pkt, <<>>) of
+	<<"r">> ->
+	    R = dict_get(<<"r">>, Pkt, []),
+	    handle_reply(Addr, T, {ok, R}, State);
+	<<"e">> ->
+	    E = dict_get(<<"e">>, Pkt, []),
+	    handle_reply(Addr, T, {error, E}, State);
+	<<"q">> ->
+	    spawn_link(fun() ->
+			       Q = dict_get(<<"q">>, Pkt, <<>>),
+			       A = dict_get(<<"a">>, Pkt, []),
+			       io:format("question from ~s: ~p~n", [addr_to_s(Addr), Q]),
+			       handle_question(Addr, T, Q, A, State)
+		       end),
+	    next_state(noreply, State);
+	_ ->
+	    next_state(noreply, State)
+    end.
 
-handle_info({udp, Sock, IP, Port, Packet},
-	    #state{sock = Sock, requests = Requests, node_id = NodeId} = State) ->
-    Addr = ip_port_to_addr(IP, Port),
-    case (catch benc:parse(Packet)) of
-	{'EXIT', Reason} ->
-	    io:format("Received garbage from ~p: ~p", [Addr, Reason]);
-	Reply ->
-	    T = dict_get(<<"t">>, Reply, <<>>),
-	    Result =
-		case dict_get(<<"y">>, Reply, <<>>) of
-		    <<"r">> ->
-			case dict_get(<<"r">>, Reply, false) of
-			    false -> {ok, []};
-			    R ->
-				see_r(Addr, R),
-				see_r_nodes(R),
-				{ok, R}
-			end;
-		    <<"e">> ->
-			{error, dict_get(<<"e">>, Reply, [])};
-		    <<"q">> ->
-			Q = dict_get(<<"q">>, Reply, <<>>),
-			As1 = dict_get(<<"a">>, Reply, []),
-			io:format("question from ~p: ~p~n", [Addr, Q]),
-			{question, Q, As1};
-		    _ -> {error, []}
-		end,
+handle_info({udp, Sock, IP, Port, PktBin}, #state{sock = Sock} = State) ->
+    I = self(),
+    spawn_link(
+      fun() ->
+	      Addr = ip_port_to_addr(IP, Port),
+	      case (catch benc:parse(PktBin)) of
+		  {'EXIT', Reason} ->
+		      io:format("Received garbage from ~s: ~p", [addr_to_s(Addr), Reason]);
+		  Pkt ->
+		      gen_server:cast(I, {packet, Addr, Pkt})
+	      end
+      end),
+    next_state(noreply, State);
 
-	    case Result of
-
-		{question, <<"ping">>, As2} ->
-		    NodeId1 = dict_get(<<"id">>, As2, <<>>),
-		    nodes:seen(Addr, NodeId1, unsure),
-		    Pkt = [{<<"t">>, T},
-			   {<<"y">>, <<"r">>},
-			   {<<"r">>,
-			    [{<<"id">>, NodeId}]
-			   }],
-		    gen_udp:send(Sock, IP, Port, benc:to_binary(Pkt));
-
-		{question, <<"find_node">>, As2} ->
-		    NodeId1 = dict_get(<<"id">>, As2, <<>>),
-		    nodes:seen(Addr, NodeId1, unsure),
-		    Target = dict_get(<<"target">>, As2, <<>>),
-		    CompactNodes = list_to_binary(nodes:get_nearest(Target, good)),
-		    Pkt = [{<<"t">>, T},
-			   {<<"y">>, <<"r">>},
-			   {<<"r">>,
-			    [{<<"id">>, NodeId},
-			     {<<"nodes">>, CompactNodes}]
-			   }],
-		    gen_udp:send(Sock, IP, Port, benc:to_binary(Pkt));
-
-		{question, _, _} ->
-		    Pkt = [{<<"t">>, T},
-			   {<<"y">>, <<"e">>},
-			   {<<"e">>, [204, <<"I didn't hear you. Lala lala la.">>]
-			   }],
-		    gen_udp:send(Sock, IP, Port, benc:to_binary(Pkt));
-
-		_ ->
-		    case ets:lookup(Requests, T) of
-			[#request{caller = Caller}] ->
-			    ets:delete(Requests, T),
-			    gen_server:reply(Caller, Result);
-			[] ->
-			    %% Late?
-			    io:format("Unexpected packet from ~p with T=~p~n", [Addr, T]),
-			    ignore
-		    end
-
-	    end
-    end,
-    {noreply, State, ?TICK_INTERVAL};
-
-handle_info(timeout, #state{requests = Requests, node_id = NodeId} = State) ->
+handle_info(timeout, #state{requests = Requests} = State) ->
     Now = util:mk_timestamp_ms(),
     TimedOut = ets:foldl(fun(#request{last_sent = LastSent} = Req, L)
 			    when LastSent + ?PKT_TIMEOUT =< Now ->
@@ -223,10 +120,8 @@ handle_info(timeout, #state{requests = Requests, node_id = NodeId} = State) ->
 			    (_, L) -> L
 			 end, [], Requests),
     Resends = lists:foldl(
-		fun(#request{caller = Caller, ntry = Ntry,
-			     addr = Addr} = Req, Resends)
+		fun(#request{caller = Caller, ntry = Ntry} = Req, Resends)
 		   when Ntry >= ?MAX_TRIES ->
-			nodes:seen(Addr, <<>>, bad),
 			ets:delete(Requests, Req#request.t),
 			gen_server:reply(Caller, timeout),
 			Resends;
@@ -238,24 +133,13 @@ handle_info(timeout, #state{requests = Requests, node_id = NodeId} = State) ->
 		end, 0, TimedOut),
     if
 	Resends > 0 ->
-	    nothing;
+	    next_state(noreply, State);
 	true ->
-	    lists:foreach(
-	      fun({Addr1, NodeId1}) ->
-		      I = self(),
-		      spawn_link(
-			fun() ->
-				catch gen_server:call(I,
-						      {request, Addr1,
-						       <<"find_node">>, [{<<"target">>, NodeId1}]},
-						      ?CALL_TIMEOUT)
-			end)
-	      end, nodes:neighbors_to_maintain(NodeId, 8))
-    end,
-    {noreply, State, ?TICK_INTERVAL};
+	    next_state(noreply, State)
+    end;
 
 handle_info(_Info, State) ->
-    {noreply, State, ?TICK_INTERVAL}.
+    next_state(noreply, State).
 
 terminate(_Reason, _State) ->
     ok.
@@ -266,6 +150,40 @@ code_change(_OldVsn, State, _Extra) ->
 %%--------------------------------------------------------------------
 %%% Internal functions
 %%--------------------------------------------------------------------
+
+next_state(Result, State) ->
+    {Result, State, 100}.
+
+handle_reply(Addr, T, Result, #state{requests = Requests} = State) ->
+    case ets:lookup(Requests, T) of
+	[#request{caller = Caller}] ->
+	    ets:delete(Requests, T),
+	    gen_server:reply(Caller, Result);
+	[] ->
+	    %% Late?
+	    io:format("Unexpected packet from ~p with T=~p~n", [addr_to_s(Addr), T]),
+	    ignore
+    end,
+    next_state(noreply, State).
+
+
+handle_question(Addr, T, <<"ping">>, _As, #state{node_id = NodeId,
+						 sock = Sock}) ->
+    {IP, Port} = addr_to_ip_port(Addr),
+    Pkt = [{<<"t">>, T},
+	   {<<"y">>, <<"r">>},
+	   {<<"r">>,
+	    [{<<"id">>, NodeId}]
+	   }],
+    gen_udp:send(Sock, IP, Port, benc:to_binary(Pkt));
+
+handle_question(Addr, T, _Q, _As, #state{sock = Sock}) ->
+    {IP, Port} = addr_to_ip_port(Addr),
+    Pkt = [{<<"t">>, T},
+	   {<<"y">>, <<"e">>},
+	   {<<"e">>, [204, <<"I didn't hear you. Lala lala la.">>]
+	   }],
+    gen_udp:send(Sock, IP, Port, benc:to_binary(Pkt)).
 
 dict_get(Key, Dict, Default) ->
     case lists:keysearch(Key, 1, Dict) of
@@ -284,21 +202,6 @@ next_t(T1) when T1 > 16#FFFF ->
 next_t(T1) ->
     T = <<((T1 bsr 16) band 16#FF):8, (T1 band 16#FF):8>>,
     {T, T1 + 1}.
-
-see_r(Addr, R) ->
-    case dict_get(<<"id">>, R, false) of
-	false -> ignore;
-	NodeId -> nodes:seen(Addr, NodeId)
-    end.
-
-see_r_nodes(R) ->
-    case dict_get(<<"nodes">>, R, false) of
-	false -> ignore;
-	Nodes ->
-	    lists:foreach(fun({Addr, NodeId}) ->
-				  nodes:seen(Addr, NodeId, unsure)
-			  end, split_contact_nodes(Nodes))
-    end.
 
 
 split_contact_nodes(<<PeerId:20/binary, Addr:6/binary, Rest/binary>>) ->
---
+++ routes.erl
@@ -0,0 +1,179 @@
+-module(routes).
+
+-export([new/1, insert/3, mark/4, pinged/2, next_action/1]).
+
+-record(routes, {node_id, buckets = []}).
+-record(bucket, {order,
+		 peers = [],
+		 cached_peers = []
+		}).
+-record(peer, {addr,
+	       node_id,
+	       status = unsure,
+	       last_ping = 0}).
+
+-define(BUCKET_SIZE, 8).
+-define(CACHE_MAX, 32).
+-define(PING_INTERVAL, 15 * 60 * 1000).
+-define(LONG_DELAY, 60 * 1000).  %% infinity is not easily comparable
+
+%%====================================================================
+%% API
+%%====================================================================
+
+new(NodeId) ->
+    Buckets = [#bucket{order = Order}
+	       || Order <- lists:seq(1, 160)],
+    #routes{node_id = NodeId,
+	   buckets = Buckets}.
+
+insert(Routes, Addr1, NodeId1) ->
+    bucket_op(Routes, NodeId1,
+	      fun(#bucket{peers = Peers,
+			  cached_peers = CachedPeers} = Bucket) ->
+		      if
+			  length(CachedPeers) < ?CACHE_MAX ->
+			      KnowAlready = lists:any(fun(#peer{addr = Addr2,
+								node_id = NodeId2}) ->
+							      Addr1 == Addr2 andalso NodeId1 == NodeId2
+						      end, Peers ++ CachedPeers),
+			      case KnowAlready of
+				  false ->
+				      Bucket#bucket{cached_peers = [#peer{addr = Addr1,
+									  node_id = NodeId1} | CachedPeers]};
+				  true -> ignore
+			      end;
+			  true -> ignore
+		      end
+	      end).
+
+mark(Routes, Addr1, NodeId1, NewStatus)
+  when NewStatus == good; NewStatus == bad ->
+    UpdateFun = fun(#peer{addr = Addr2,
+			  node_id = NodeId2} = Peer)
+		   when Addr1 == Addr2 andalso NodeId1 == NodeId2 ->
+			Peer#peer{status = NewStatus};
+		   (Peer) -> Peer
+		end,
+    bucket_op(Routes, NodeId1,
+	      fun(#bucket{peers = Peers1,
+			  cached_peers = CachedPeers1} = Bucket1) ->
+		      Peers2 = lists:map(UpdateFun, Peers1),
+		      CachedPeers2 = lists:map(UpdateFun, CachedPeers1),
+
+		      maintain_bucket(Bucket1#bucket{peers = Peers2,
+						     cached_peers = CachedPeers2})
+	      end).
+
+pinged(Routes, NodeId1) ->
+    Now = util:mk_timestamp_ms(),
+    UpdateFun = fun(#peer{node_id = NodeId2} = Peer)
+		   when NodeId1 == NodeId2 ->
+			Peer#peer{last_ping = Now}
+		end,
+    bucket_op(Routes, NodeId1,
+	      fun(#bucket{peers = Peers,
+			  cached_peers = CachedPeers} = Bucket) ->
+		      Bucket#bucket{peers = lists:map(UpdateFun, Peers),
+				    cached_peers = lists:map(UpdateFun, CachedPeers)}
+	      end).
+
+-record(action, {time, action}).
+
+%% returns {wait, NextInMS} | {ping, NodeId, Addr}
+next_action(#routes{buckets = Buckets}) ->
+    Actions = lists:map(fun next_action1/1, Buckets),
+    Now = util:mk_timestamp_ms(),
+    case soonest_action(Actions) of
+	#action{time = infinity} ->
+	    {wait, infinity};
+	#action{time = Time, action = Action} when Time =< Now ->
+	    Action;
+	#action{time = Time} ->
+	    {wait, Time - Now}
+    end.
+
+next_action1(#bucket{peers = Peers}) ->
+    PeerActions = [#action{time = LastPing + ?PING_INTERVAL,
+			   action = {ping, NodeId}}
+		   || #peer{node_id = NodeId,
+			    last_ping = LastPing} <- Peers],
+    CachedPeersActions = if
+			     length(Peers) < ?BUCKET_SIZE ->
+				 [#action{time = LastPing + ?PING_INTERVAL,
+					  action = {ping, NodeId, Addr}}
+				  || #peer{node_id = NodeId,
+					   addr = Addr,
+					   last_ping = LastPing} <- Peers];
+			     true -> []
+			 end,
+    soonest_action([soonest_action(PeerActions) | CachedPeersActions]).
+
+soonest_action(Actions) ->
+    lists:foldl(fun(#action{time = infinity}, Action) ->
+			Action;
+		   (Action, #action{time = infinity}) ->
+			Action;
+		   (#action{time = Time1} = Action, #action{time = Time2})
+		   when Time1 < Time2 ->
+			Action;
+		   (#action{}, #action{} = Action) ->
+			Action
+		end, #action{time = infinity, action = nothing}, Actions).
+			
+
+%%--------------------------------------------------------------------
+%%% Internal functions
+%%--------------------------------------------------------------------
+
+bucket_op(#routes{node_id = NodeId,
+		  buckets = Buckets1} = Routes, NodeId1, BucketFun) ->
+    Order = distance_order(NodeId, NodeId1),
+    Bucket1 = case lists:keysearch(Order, #bucket.order, Buckets1) of
+		  {value, #bucket{} = B} -> B;
+		  false -> #bucket{order = Order}
+	      end,
+    Bucket2 = BucketFun(Bucket1),
+    Buckets2 = lists:keystore(Order, #bucket.order, Buckets1, Bucket2),
+    Routes#routes{buckets = lists:sort(Buckets2)}.
+
+
+maintain_bucket(#bucket{peers = Peers1,
+			cached_peers = CachedPeers1} = Bucket) ->
+    %% move not-good peers to cached_peers
+    {Peers2, NewCachedPeers} =
+	lists:foldl(fun(#peer{status = good} = Peer, {StillGood, NotGood}) ->
+			    {[Peer | StillGood], NotGood};
+		       (Peer, {StillGood, NotGood}) ->
+			    {StillGood, [Peer | NotGood]}
+		    end, {[], []}, Peers1),
+    %% move good peers to peers
+    CachedPeers2 = NewCachedPeers ++ CachedPeers1,
+    {Peers3, CachedPeers3} =
+	lists:foldl(fun(#peer{status = good} = Peer, {Peers, CachedPeers})
+		       when length(Peers) < ?BUCKET_SIZE ->
+			    {[Peer | Peers], CachedPeers};
+		       (Peer, {Peers, CachedPeers}) ->
+			    {Peers, [Peer | CachedPeers]}
+		    end, {Peers2, []}, CachedPeers2),
+
+    Bucket#bucket{peers = Peers3,
+		  cached_peers = CachedPeers3}.
+
+%% sort_by_distance(InfoHash, Nodes) ->
+%%     lists:sort(fun(#node{node_id = NodeId1},
+%% 		   #node{node_id = NodeId2}) ->
+%% 		       distance(InfoHash, NodeId1) =< distance(InfoHash, NodeId2)
+%% 	       end, Nodes).
+
+distance(<<A:160/big-unsigned>>, <<B:160/big-unsigned>>) ->
+    A bxor B;
+distance(_, _) ->
+    16#FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF.
+
+distance_order(A, B) ->
+    Distance = distance(A, B),
+    distance_order1(Distance, 0).
+distance_order1(0, Order) -> Order;
+distance_order1(Distance, Order) ->
+    distance_order1(Distance bsr 1, Order + 1).
