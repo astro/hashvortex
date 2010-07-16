578ebe8fa60bd1fedcf50444ed9c44de9514d939
Astro <astro@spaceboyz.net>
2010-07-16 15:19:33 +0200

    peer.js: running multiple peers support

--- peer.js
+++ peer.js
@@ -43,6 +43,20 @@ function encodeNodes(nodes) {
     return result;
 }
 
+var peers = [];
+function myNearestNodeId(nodeid) {
+    var minDistance = 160;
+    var result = nodeid;
+    peers.forEach(function(peer) {
+	var distance = NodeId.distance(nodeid, peer.nodeid);
+	if (distance < minDistance) {
+	    minDistance = distance;
+	    result = peer.nodeid;
+	}
+    });
+    return result;
+}
+
 var node = new KRPC.Node(10000);
 var queryCnt = 0, replyCnt = 0;
 var token = new Buffer('a');
@@ -54,12 +68,15 @@ node.on('query', function(addr, port, pkt, reply) {
 
     switch(pkt.q.toString()) {
     case 'ping':
-	reply({ id: nodeid });
+	if (pkt.a.nodeid)
+	    reply({ id: myNearestNodeId(pkt.a.nodeid) });
 	break;
     case 'find_node':
-	var nodes = pkt.a.target ? NodeDB.nearest(pkt.a.target) : [];
-	reply({ id: nodeid,
-		nodes: encodeNodes(nodes) });
+	if (pkt.a.target) {
+	    var nodes = NodeDB.nearest(pkt.a.target);
+	    reply({ id: myNearestNodeId(pkt.a.target),
+		    nodes: encodeNodes(nodes) });
+	}
 	break;
     case 'get_peers':
 	reply({ id: pkt.a.info_hash || nodeid,
@@ -140,7 +157,5 @@ Peer.prototype = {
     }
 };
 
-//var nodeid = NodeId.parseMagnet("magnet:?xt=urn:btih:8874e48d1b71415af0585a424d239f324b4342eb&dn=The+Twilight+Saga+Eclipse+TS+XViD+-+IMAGiNE&tr=http%3A%2F%2Fdenis.stalker.h3q.com%3A6969%2Fannounce&tr=http%3A%2F%2Ftracker.mytorrenttracker.com%3A6099%2Fannounce");
-var nodeid = NodeId.parseMagnet("magnet:?xt=urn:btih:0777769ef49ebfef7212c71a09b67eb68bcc602d&dn=Hot+Tub+Time+Machine+%282010%29+R5+XviD-MAX&tr=http%3A%2F%2Ftracker.prq.to%2Fannounce&tr=http%3A%2F%2Ftracker.mytorrenttracker.com%3A6099%2Fannounce");
-console.log("nodeid: " + JSON.stringify(nodeid));
-new Peer(nodeid, node);
+peers.push(new Peer(NodeId.parseMagnet("magnet:?xt=urn:btih:8874e48d1b71415af0585a424d239f324b4342eb&dn=The+Twilight+Saga+Eclipse+TS+XViD+-+IMAGiNE&tr=http%3A%2F%2Fdenis.stalker.h3q.com%3A6969%2Fannounce&tr=http%3A%2F%2Ftracker.mytorrenttracker.com%3A6099%2Fannounce"), node));
+peers.push(new Peer(NodeId.parseMagnet("magnet:?xt=urn:btih:0777769ef49ebfef7212c71a09b67eb68bcc602d&dn=Hot+Tub+Time+Machine+%282010%29+R5+XviD-MAX&tr=http%3A%2F%2Ftracker.prq.to%2Fannounce&tr=http%3A%2F%2Ftracker.mytorrenttracker.com%3A6099%2Fannounce"), node));
